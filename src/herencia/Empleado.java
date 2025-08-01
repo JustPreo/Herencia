180ef35b68bf8738722aa6aa66bee4b78ed4e88e
JustPreo <impre0hn@gmail.com>
2025-08-01 10:14:41 -0600

    a

--- src/herencia/Empleado.java
+++ src/herencia/Empleado.java
@@ -47,7 +47,9 @@ public class Empleado {
         //Devuelve una cadena que incluye el código, el nombre y la fecha de contratación del empleado.
         return "Codigo:"+codigo
                 +"\nNombre completo:"+nombre+""
-                + "\nFecha Contratacion:"+fechaContratacion;
+                + "Fecha contratacion:" + fechaContratacion.get(Calendar.MONTH)
+                + "/" + fechaContratacion.get(Calendar.DAY_OF_MONTH)
+                + "/" + fechaContratacion.get(Calendar.YEAR);
     }
 
 }
